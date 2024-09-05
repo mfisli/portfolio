0611d73a5e62353d9da2bea299a38f86cbeb3bc8
Maks <mfisli2@gmail.com>
2024-09-05 12:25:52 -0700

    init

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+export const collections = {
+	work: defineCollection({
+		type: 'content',
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
 	}),
-});
-
-export const collections = { blog };
+};
